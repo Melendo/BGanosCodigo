d8f63ae97254638203de9f1a76b73cf9fa54ebd0
TenshiZ <angelant@ucm.es>
2024-10-24 13:22:09 +0200

    ApplicationControllerImp terminado

--- BGanosCOD/src/Presentacion/Controller/IGUI.java
+++ BGanosCOD/src/Presentacion/Controller/IGUI.java
@@ -5,18 +5,8 @@ package Presentacion.Controller;
 
 import Presentacion.Controller.Command.Context;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author airam
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
+
 public interface IGUI {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param context
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	public void actualizar(Context context);
 }
