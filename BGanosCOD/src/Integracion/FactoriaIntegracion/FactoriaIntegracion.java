a0a87f995b240c8d1c17648ed1ee58011bb710cc
TenshiZ <angelant@ucm.es>
2024-10-24 13:04:19 +0200

    FactoriaIntegracion terminado

--- BGanosCOD/src/Integracion/FactoriaIntegracion/FactoriaIntegracion.java
+++ BGanosCOD/src/Integracion/FactoriaIntegracion/FactoriaIntegracion.java
@@ -1,131 +1,37 @@
-/**
- * 
- */
+
 package Integracion.FactoriaIntegracion;
 
-import Integracion.Invernadero.InvernaderoDAO;
+import  Integracion.Invernadero.InvernaderoDAO;
 import Integracion.Entrada.EntradaDAO;
 import Integracion.Fabricante.FabricanteDAO;
 import Integracion.Factura.FacturaDAO;
 import Integracion.Planta.PlantaDAO;
 import Integracion.SistemaDeRiego.SistemaDeRiegoDAO;
-import Integracion.Invernadero.TieneDAO;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author airam
-* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-*/
-public class FactoriaIntegracion {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
+
+public  abstract class FactoriaIntegracion {
+	
 	private static FactoriaIntegracion instance;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	public static FactoriaIntegracion getInstance() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		if(instance == null)
+			instance = new FactoriaIntegracionImp();
+		return instance;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public InvernaderoDAO createInvernaderoDAO() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public EntradaDAO createEntradaDAO() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public FabricanteDAO createFabricanteDAO() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public FacturaDAO createFacturaDAO() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public PlantaDAO crearePlantaDAO() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public SistemaDeRiegoDAO createSistemaDeRiesoDAO() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public TieneDAO createTieneDAO() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
+	public abstract FacturaDAO getFacturaDAO();
+	
+	public abstract EntradaDAO getEntradaDAO();
+	
+	public abstract InvernaderoDAO getInvernaderoDAO();
+	
+	public abstract SistemaDeRiegoDAO getSistemaDeRiegoDAO();
+	
+	public abstract FabricanteDAO getFabricanteDAO();
+	
+	public abstract PlantaDAO getPlantaDAO();
+	
+	
 }
