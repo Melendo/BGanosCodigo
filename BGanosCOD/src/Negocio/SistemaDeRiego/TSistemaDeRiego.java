d472ec44bf5bb9a04badc38fed5dc9321128d1fc
TenshiZ <angelant@ucm.es>
2024-10-24 19:39:13 +0200

    TsistemaDeRiego

--- BGanosCOD/src/Negocio/SistemaDeRiego/TSistemaDeRiego.java
+++ BGanosCOD/src/Negocio/SistemaDeRiego/TSistemaDeRiego.java
@@ -3,12 +3,6 @@
  */
 package Negocio.SistemaDeRiego;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author airam
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public class TSistemaDeRiego {
 	
 	private Integer id;
