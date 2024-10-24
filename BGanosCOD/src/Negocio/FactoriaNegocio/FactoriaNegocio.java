f5d5a603a6664e69b4c11d1930179aebe0a64316
TenshiZ <angelant@ucm.es>
2024-10-24 13:13:29 +0200

    FactoriaNegocio terminado

--- BGanosCOD/src/Negocio/FactoriaNegocio/FactoriaNegocio.java
+++ BGanosCOD/src/Negocio/FactoriaNegocio/FactoriaNegocio.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package Negocio.FactoriaNegocio;
 
 import Negocio.Entrada.EntradaSA;
@@ -10,108 +8,26 @@ import Negocio.Invernadero.InvernaderoSA;
 import Negocio.Planta.PlantaSA;
 import Negocio.SistemaDeRiego.SistemaDeRiegoSA;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author airam
-* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-*/
-public class FactoriaNegocio {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+public abstract class FactoriaNegocio {
+
 	private static FactoriaNegocio instance;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public static FactoriaNegocio getInstance() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		if(instance == null) 
+			instance = new FactoriaSAImp();
+		return instance;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public EntradaSA getEntradaSA() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
+	public abstract EntradaSA getEntradaSA();
+	
+	public abstract FabricanteSA getFabricanteSA();
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public FabricanteSA getFabricanteSA() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
+	public abstract FacturaSA getFacturaSA();
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public FacturaSA getFacturaSA() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
+	public abstract  InvernaderoSA getInvernaderoSA();
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public InvernaderoSA getInvernaderoSA() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
+	public abstract PlantaSA getPlantaSA();
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public PlantaSA getPlantaSA() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public SistemaDeRiegoSA getSistemaDeRiegoSA() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
+	public abstract SistemaDeRiegoSA getSistemaDeRiegoSA();
+	
 }
