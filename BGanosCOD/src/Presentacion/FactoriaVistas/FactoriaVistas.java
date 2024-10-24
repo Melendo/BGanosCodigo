41c8dcbaa4ec332266e3cd463237670865e0537a
TenshiZ <angelant@ucm.es>
2024-10-24 11:49:21 +0200

    FactoriaVistas

--- BGanosCOD/src/Presentacion/FactoriaVistas/FactoriaVistas.java
+++ BGanosCOD/src/Presentacion/FactoriaVistas/FactoriaVistas.java
@@ -1,49 +1,20 @@
-/**
- * 
- */
+
 package Presentacion.FactoriaVistas;
 
 import Presentacion.Controller.IGUI;
 import Presentacion.Controller.Command.Context;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author airam
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
-public class FactoriaVistas {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+public abstract class FactoriaVistas {
+	
 	private static FactoriaVistas instance;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public static FactoriaVistas getIntance() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		if(instance == null)
+		{
+			instance = new FactoriaVistasImp();
+		}
+		return instance;
 	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param context
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public IGUI generarVista(Context context) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
+	
+	public abstract IGUI generarVista(Context context);
 }
