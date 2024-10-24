7ba1d8d69bb8af116919b15f2c062c050aeb7965
TenshiZ <angelant@ucm.es>
2024-10-24 12:12:05 +0200

    base Evento y FactoriaVistasImp

--- BGanosCOD/src/Presentacion/FactoriaVistas/Evento.java
+++ BGanosCOD/src/Presentacion/FactoriaVistas/Evento.java
@@ -1,13 +1,70 @@
-/**
- * 
- */
+
 package Presentacion.FactoriaVistas;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author airam
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
+
 public class Evento {
+    
+    // Vistas Principales
+    public static final int VISTA_PRINCIPAL = 100;
+    
+	//	Entrada
+	    
+	//				Vistas Entrada
+	public static final int ENTRADA_VISTA = 200;
+	public static final int ALTA_ENTRADA_VISTA = 201;
+	public static final int BAJA_ENTRADA_VISTA = 202;
+	public static final int MODIFICAR_ENTRADA_VISTA = 203;
+	public static final int MOSTRAR_ENTRADA_VISTA = 204;
+	public static final int LISTAR_ENTRADAS_VISTA = 205;
+	public static final int LISTAR_ENTRADAS_POR_TIPO_VISTA = 206;
+	
+	//				Fabricante
+	
+	//				Vistas Fabricante
+	public static final int FABRICANTE_VISTA = 300;
+	public static final int ALTA_FABRICANTE_VISTA = 301;
+	public static final int BAJA_FABRICANTE_VISTA = 302;
+	public static final int MODIFICAR_FABRICANTE_VISTA = 303;
+	public static final int MOSTRAR_FABRICANTE_VISTA = 304;
+	public static final int LISTAR_FABRICANTES_VISTA = 305;
+	
+	//				Factura
+	
+	//				Vistas Factura
+	public static final int FACTURA_VISTA = 400;
+	public static final int ABRIR_FACTURA_VISTA = 401;
+	public static final int DEVOLVER_FACTURA_VISTA = 402;
+	public static final int MODIFICAR_FACTURA_VISTA = 403;
+	public static final int MOSTRAR_FACTURA_VISTA = 404;
+	public static final int LISTAR_FACTURAS_VISTA = 405;
+	
+	//				Invernadero
+	
+	//				Vistas Invernadero
+	public static final int INVERNADERO_VISTA = 500;
+	public static final int ALTA_INVERNADERO_VISTA = 501;
+	public static final int BAJA_INVERNADERO_VISTA = 502;
+	public static final int MODIFICAR_INVERNADERO_VISTA = 503;
+	public static final int MOSTRAR_INVERNADERO_VISTA = 504;
+	public static final int LISTAR_INVERNADEROS_VISTA = 505;
+	
+	//				Planta
+	
+	//				Vistas Planta
+	public static final int PLANTA_VISTA = 600;
+	public static final int ALTA_PLANTA_VISTA = 601;
+	public static final int BAJA_PLANTA_VISTA = 602;
+	public static final int MODIFICAR_PLANTA_VISTA = 603;
+	public static final int MOSTRAR_PLANTA_VISTA = 604;
+	public static final int LISTAR_PLANTAS_VISTA = 605;
+	
+	//				Sistema de Riego
+	
+	//				Vistas Sistema de Riego
+	public static final int SISTEMA_RIEGO_VISTA = 700;
+	public static final int ALTA_SISTEMA_RIEGO_VISTA = 701;
+	public static final int BAJA_SISTEMA_RIEGO_VISTA = 702;
+	public static final int MODIFICAR_SISTEMA_RIEGO_VISTA = 703;
+	public static final int MOSTRAR_SISTEMA_RIEGO_VISTA = 704;
+	public static final int LISTAR_SISTEMAS_RIEGO_VISTA = 705;
 }
--- BGanosCOD/src/Presentacion/FactoriaVistas/FactoriaVistasImp.java
+++ BGanosCOD/src/Presentacion/FactoriaVistas/FactoriaVistasImp.java
@@ -1,13 +1,158 @@
-/**
- * 
- */
 package Presentacion.FactoriaVistas;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author airam
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
+import Presentacion.Controller.IGUI;
+import Presentacion.Controller.Command.Context;
+
 public class FactoriaVistasImp extends FactoriaVistas {
+	IGUI vistaActual = null;
+
+	@SuppressWarnings("unchecked")
+	public IGUI generarVista(Context contexto) {
+		switch(contexto.getEvento())
+		{
+		
+		// 				Vista Principal
+			case Evento.VISTA_PRINCIPAL:
+				vistaActual = new VistaPrincipal();
+				return vistaActual;
+		
+		// 				Vistas Entrada
+				
+			case Evento.ENTRADA_VISTA:
+				vistaActual = new VistaEntrada();
+				return vistaActual;
+			case Evento.ALTA_ENTRADA_VISTA:
+				vistaActual = new VistaAltaEntrada();
+				return vistaActual;
+			case Evento.BAJA_ENTRADA_VISTA:
+				vistaActual = new VistaBajaEntrada();
+				return vistaActual;
+			case Evento.MODIFICAR_ENTRADA_VISTA:
+				vistaActual = new VistaModificarEntrada();
+				return vistaActual;
+			case Evento.MOSTRAR_ENTRADA_VISTA:
+				vistaActual = new VistaMostrarEntrada();
+				return vistaActual;
+			case Evento.LISTAR_ENTRADAS_VISTA:
+                vistaActual = new VistaListarEntradas((Set<TEntrada>) contexto.getDatos());
+				return vistaActual;
+			case Evento.LISTAR_ENTRADAS_POR_TIPO_VISTA:
+				vistaActual = new VistaListarEntradasPorTipo();
+				return vistaActual;
+
+
+		// 					Vistas Fabricante
+				
+		case Evento.FABRICANTE_VISTA:
+			vistaActual = new VistaFabricante();
+			return vistaActual;
+		case Evento.ALTA_FABRICANTE_VISTA:
+			vistaActual = new VistaAltaFabricante();
+			return vistaActual;
+		case Evento.BAJA_FABRICANTE_VISTA:
+			vistaActual = new VistaBajaFabricante();
+			return vistaActual;
+		case Evento.MODIFICAR_FABRICANTE_VISTA:
+			vistaActual = new VistaModificarFabricante();
+			return vistaActual;
+		case Evento.MOSTRAR_FABRICANTE_VISTA:
+			vistaActual = new VistaMostrarFabricante();
+			return vistaActual;
+		case Evento.LISTAR_FABRICANTES_VISTA:
+			vistaActual = new VistaListarFabricantes((Set<TFabricante>) contexto.getDatos());
+			return vistaActual;
+			
+	
+			
+		// 				Vistas Factura
+			
+		case Evento.FACTURA_VISTA:
+			vistaActual = new VistaFactura();
+			return vistaActual;
+		case Evento.ABRIR_FACTURA_VISTA:
+			vistaActual = new VistaAbrirFactura();
+			return vistaActual;
+		case Evento.DEVOLVER_FACTURA_VISTA:
+			vistaActual = new VistaDevolverFactura();
+			return vistaActual;
+		case Evento.MODIFICAR_FACTURA_VISTA:
+			vistaActual = new VistaModificarFactura();
+			return vistaActual;
+		case Evento.MOSTRAR_FACTURA_VISTA:
+			vistaActual = new VistaMostrarFactura();
+			return vistaActual;
+		case Evento.LISTAR_FACTURAS_VISTA:
+			vistaActual = new VistaListarFacturas((Set<TFactura>) contexto.getDatos());
+			return vistaActual;
+			
+			
+		//				Vistas Invernadero
+			
+		case Evento.INVERNADERO_VISTA:
+			vistaActual = new VistaInvernadero();
+			return vistaActual;
+		case Evento.ALTA_INVERNADERO_VISTA:
+			vistaActual = new VistaAltaInvernadero();
+			return vistaActual;
+		case Evento.BAJA_INVERNADERO_VISTA:
+			vistaActual = new VistaBajaInvernadero();
+			return vistaActual;
+		case Evento.MODIFICAR_INVERNADERO_VISTA:
+			vistaActual = new VistaModificarInvernadero();
+			return vistaActual;
+		case Evento.MOSTRAR_INVERNADERO_VISTA:
+			vistaActual = new VistaMostrarInvernadero();
+			return vistaActual;
+		case Evento.LISTAR_INVERNADEROS_VISTA:
+			vistaActual = new VistaListarInvernaderos((Set<TInvernadero>) contexto.getDatos());
+			return vistaActual;
+			
+
+		// 				Vistas Planta
+			
+		case Evento.PLANTA_VISTA:
+			vistaActual = new VistaPlanta();
+			return vistaActual;
+		case Evento.ALTA_PLANTA_VISTA:
+			vistaActual = new VistaAltaPlanta();
+			return vistaActual;
+		case Evento.BAJA_PLANTA_VISTA:
+			vistaActual = new VistaBajaPlanta();
+			return vistaActual;
+		case Evento.MODIFICAR_PLANTA_VISTA:
+			vistaActual = new VistaModificarPlanta();
+			return vistaActual;
+		case Evento.MOSTRAR_PLANTA_VISTA:
+			vistaActual = new VistaMostrarPlanta();
+			return vistaActual;
+		case Evento.LISTAR_PLANTAS_VISTA:
+			vistaActual = new VistaListarPlantas((Set<TPlanta>) contexto.getDatos());
+			return vistaActual;
+			
+
+		// 				Vistas Sistema de Riego
+			
+		case Evento.SISTEMA_RIEGO_VISTA:
+			vistaActual = new VistaSistemaRiego();
+			return vistaActual;
+		case Evento.ALTA_SISTEMA_RIEGO_VISTA:
+			vistaActual = new VistaAltaSistemaRiego();
+			return vistaActual;
+		case Evento.BAJA_SISTEMA_RIEGO_VISTA:
+			vistaActual = new VistaBajaSistemaRiego();
+			return vistaActual;
+		case Evento.MODIFICAR_SISTEMA_RIEGO_VISTA:
+			vistaActual = new VistaModificarSistemaRiego();
+			return vistaActual;
+		case Evento.MOSTRAR_SISTEMA_RIEGO_VISTA:
+			vistaActual = new VistaMostrarSistemaRiego();
+			return vistaActual;
+		case Evento.LISTAR_SISTEMAS_RIEGO_VISTA:
+			vistaActual = new VistaListarSistemasRiego((Set<TSistemaRiego>) contexto.getDatos());
+			return vistaActual;
+
+		default:
+			return null;
+		}
+	}
 }
