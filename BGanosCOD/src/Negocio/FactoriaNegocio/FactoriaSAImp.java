95bbad4a32b2ffe8d123cc80fe6a4edf8cc9dd65
TenshiZ <angelant@ucm.es>
2024-10-24 13:15:55 +0200

    FactoriaSAImp terminado

--- BGanosCOD/src/Negocio/FactoriaNegocio/FactoriaSAImp.java
+++ BGanosCOD/src/Negocio/FactoriaNegocio/FactoriaSAImp.java
@@ -1,13 +1,43 @@
-/**
- * 
- */
+
 package Negocio.FactoriaNegocio;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author airam
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
+import Negocio.Entrada.EntradaSA;
+import Negocio.Entrada.EntradaSAImp;
+import Negocio.Fabricante.FabricanteSA;
+import Negocio.Fabricante.FabricanteSAImp;
+import Negocio.Factura.FacturaSA;
+import Negocio.Factura.FacturaSAImp;
+import Negocio.Invernadero.InvernaderoSA;
+import Negocio.Invernadero.InvernaderoSAImp;
+import Negocio.Planta.PlantaSA;
+import Negocio.Planta.PlantaSAImp;
+import Negocio.SistemaDeRiego.SistemaDeRiegoSA;
+import Negocio.SistemaDeRiego.SistemaDeRiegoSAImp;
+
+
 public class FactoriaSAImp extends FactoriaNegocio {
+
+	public EntradaSA getEntradaSA() {
+		return new EntradaSAImp();
+	}
+
+	public FabricanteSA getFabricanteSA() {
+		return new FabricanteSAImp();
+	}
+
+	public FacturaSA getFacturaSA() {
+		return new FacturaSAImp();
+	}
+
+	public InvernaderoSA getInvernaderoSA() {
+		return new InvernaderoSAImp();
+	}
+
+	public PlantaSA getPlantaSA() {
+		return new PlantaSAImp();
+	}
+
+	public SistemaDeRiegoSA getSistemaDeRiegoSA() {
+		return new SistemaDeRiegoSAImp();
+	}
 }
