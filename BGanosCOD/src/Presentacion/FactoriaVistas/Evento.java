a496932d2b34d4674a49d9ef6c782af70065e8d0
TenshiZ <angelant@ucm.es>
2024-10-24 14:00:41 +0200

    Evento Funciones acabado

--- BGanosCOD/src/Presentacion/FactoriaVistas/Evento.java
+++ BGanosCOD/src/Presentacion/FactoriaVistas/Evento.java
@@ -9,7 +9,7 @@ public class Evento {
     
 	//	Entrada											200 - 299
 	    
-	//				Vistas Entrada
+	//				Vistas Entrada						200 - 119
     public static final int ENTRADA_VISTA = 200;
 	public static final int ALTA_ENTRADA_VISTA = 201;
 	public static final int BAJA_ENTRADA_VISTA = 202;
@@ -18,12 +18,20 @@ public class Evento {
 	public static final int LISTAR_ENTRADAS_VISTA = 205;
 	public static final int LISTAR_ENTRADAS_INVERNADERO_VISTA = 206;
 	
+	//				Funciones Entrada					220 - 299
+	public static final int ALTA_ENTRADA = 220;
+	public static final int BAJA_ENTRADA = 221;
+	public static final int MODIFICAR_ENTRADA = 222;
+	public static final int MOSTRAR_ENTRADA_POR_ID = 223;
+	public static final int LISTAR_ENTRADAS = 224;
+	public static final int LISTAR_ENTRADAS_DE_INVERNADERO = 225;
+	
 	
 	
     
     //				Fabricante							300 - 399
     
-    //				Vistas Fabricante
+    //				Vistas Fabricante					300 - 319
 	public static final int FABRICANTE_VISTA = 300;
     public static final int ALTA_FABRICANTE_VISTA = 301;
     public static final int BAJA_FABRICANTE_VISTA = 302;
@@ -33,12 +41,22 @@ public class Evento {
     public static final int LISTAR_FABRICANTES_POR_NOMBRE_VISTA = 306;
     public static final int LISTAR_FABRICANTES_POR_TIPO_VISTA = 307;
     public static final int LISTAR_FABRICANTES_SISTEMAS_RIEGO_INVERNADERO_VISTA = 308;
+    
+    // 				Funciones Fabricante				320 - 399
+    public static final int ALTA_FABRICANTE = 320;
+    public static final int BAJA_FABRICANTE = 321;
+    public static final int MODIFICAR_FABRICANTE = 322;
+    public static final int LISTAR_FABRICANTES = 323;
+    public static final int MOSTRAR_FABRICANTE_POR_ID = 324;
+    public static final int LISTAR_FABRICANTES_POR_NOMBRE = 325;
+    public static final int LISTAR_FABRICANTES_POR_TIPO = 326;
+    public static final int LISTAR_FABRICANTES_SISTEMAS_RIEGO_INVERNADERO = 327;
 
     
     
     //				Factura									400 - 499
     
-    //				Vistas Factura
+    //				Vistas Factura							400 - 419
     public static final int FACTURA_VISTA = 400;
     public static final int ABRIR_FACTURA_VISTA = 401;
     public static final int CERRAR_FACTURA_VISTA = 402;
@@ -48,12 +66,22 @@ public class Evento {
     public static final int DEVOLVER_FACTURA_VISTA = 406;
     public static final int AÑADIR_ENTRADAS_FACTURA_VISTA = 407;
     public static final int QUITAR_ENTRADAS_FACTURA_VISTA = 408;
+    
+    //				Funciones Factura						420 - 499
+    public static final int ABRIR_FACTURA = 420;
+    public static final int CERRAR_FACTURA = 421;
+    public static final int MODIFICAR_FACTURA = 422;
+    public static final int MOSTRAR_FACTURA_POR_ID = 423;
+    public static final int LISTAR_FACTURAS = 424;
+    public static final int DEVOLVER_FACTURA = 425;
+    public static final int AÑADIR_ENTRADAS_A_FACTURA = 426;
+    public static final int QUITAR_ENTRADAS_DE_FACTURA = 427;
 
     
 
     //				Invernadero									500 - 599
     
-    //				Vistas Invernadero
+    //				Vistas Invernadero							500 - 519
     public static final int INVERNADERO_VISTA = 500;
     public static final int ALTA_INVERNADERO_VISTA = 501;
     public static final int BAJA_INVERNADERO_VISTA = 502;
@@ -65,11 +93,23 @@ public class Evento {
     public static final int DESVINCULAR_SISTEMA_RIEGO_INVERNADERO_VISTA = 508;
     public static final int LISTAR_TRES_FECHAS_MAS_ENTRADAS_INVERNADERO_VISTA = 509;
     
+    //				Funciones Invernadero						520 - 599
+    public static final int ALTA_INVERNADERO = 520;
+    public static final int BAJA_INVERNADERO = 521;
+    public static final int MODIFICAR_INVERNADERO = 522;
+    public static final int LISTAR_INVERNADEROS = 523;
+    public static final int MOSTRAR_INVERNADERO_POR_ID = 524;
+    public static final int LISTAR_INVERNADEROS_POR_SISTEMA_RIEGO = 525;
+    public static final int VINCULAR_SISTEMA_RIEGO_A_INVERNADERO = 526;
+    public static final int DESVINCULAR_SISTEMA_RIEGO_DE_INVERNADERO = 527;
+    public static final int LISTAR_TRES_FECHAS_CON_MAS_ENTRADAS_VENDIDAS = 528;
+
+    
     
     
     //				Planta										600 - 699
     
-    //				Vistas Planta
+    //				Vistas Planta								600 - 619
     public static final int PLANTA_VISTA = 600;
     public static final int ALTA_PLANTA_VISTA = 601;
     public static final int BAJA_PLANTA_VISTA = 602;
@@ -79,12 +119,20 @@ public class Evento {
     public static final int LISTAR_PLANTAS_POR_TIPO_VISTA = 606;
     public static final int LISTAR_PLANTAS_INVERNADERO_VISTA = 607;
     
+    //				Funciones Planta							620 - 699
+    public static final int ALTA_PLANTA = 620;
+    public static final int BAJA_PLANTA = 621;
+    public static final int MODIFICAR_PLANTA = 622;
+    public static final int LISTAR_PLANTAS = 623;
+    public static final int MOSTRAR_PLANTA_POR_ID = 624;
+    public static final int LISTAR_PLANTAS_POR_TIPO = 625;
+    public static final int LISTAR_PLANTAS_DE_INVERNADERO = 626;
     
     
 
     //				Sistema de Riego							700 - 799
     
-    //				Vistas Sistema de Riego
+    //				Vistas Sistema de Riego						700 - 719
     public static final int SISTEMA_RIEGO_VISTA = 700;
     public static final int ALTA_SISTEMA_RIEGO_VISTA = 701;
     public static final int BAJA_SISTEMA_RIEGO_VISTA = 702;
@@ -93,6 +141,17 @@ public class Evento {
     public static final int LISTAR_SISTEMAS_RIEGO_INVERNADERO_VISTA = 705;
     public static final int LISTAR_SISTEMAS_RIEGO_POR_FABRICANTE_VISTA = 706;
     public static final int LISTAR_SISTEMAS_RIEGO_VISTA = 707;
+    
+    //				Funciones Sistema de Riego					720 - 799
+    public static final int ALTA_SISTEMA_RIEGO = 720;
+    public static final int BAJA_SISTEMA_RIEGO = 721;
+    public static final int MODIFICAR_SISTEMA_RIEGO = 722;
+    public static final int MOSTRAR_SISTEMA_RIEGO_POR_ID = 723;
+    public static final int LISTAR_SISTEMAS_RIEGO_DE_INVERNADERO = 724;
+    public static final int LISTAR_SISTEMAS_RIEGO_POR_FABRICANTE = 725;
+    public static final int LISTAR_SISTEMAS_RIEGO = 726;
+
+	
 
 	
 
