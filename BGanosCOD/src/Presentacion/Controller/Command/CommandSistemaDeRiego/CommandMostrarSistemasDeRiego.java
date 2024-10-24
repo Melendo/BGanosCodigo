466583c3402ded627919b6cb7fad81836c254481
TenshiZ <angelant@ucm.es>
2024-10-24 19:05:26 +0200

    MostrarSistemasdeRIego Command

--- BGanosCOD/src/Presentacion/Controller/Command/CommandSistemaDeRiego/CommandMostrarSistemasDeRiego.java
+++ BGanosCOD/src/Presentacion/Controller/Command/CommandSistemaDeRiego/CommandMostrarSistemasDeRiego.java
@@ -3,25 +3,18 @@
  */
 package Presentacion.Controller.Command.CommandSistemaDeRiego;
 
+import java.util.Set;
+
+import Negocio.FactoriaNegocio.FactoriaNegocio;
+import Negocio.SistemaDeRiego.TSistemaDeRiego;
 import Presentacion.Controller.Command.Command;
 import Presentacion.Controller.Command.Context;
+import Presentacion.FactoriaVistas.Evento;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author airam
-* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-*/
 public class CommandMostrarSistemasDeRiego implements Command {
-	/** 
-	* (non-Javadoc)
-	* @see Command#execute(Object datos)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	public Context execute(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		Set<TSistemaDeRiego> resultado = FactoriaNegocio.getInstance().getSistemaDeRiegoSA().listarSisRiego();
+		return new Context(Evento.LISTAR_SISTEMAS_RIEGO, resultado);
 	}
 }
