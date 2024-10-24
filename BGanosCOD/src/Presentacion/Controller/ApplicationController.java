1adcf814625804f8fcd79359cb6d017b41ed66df
TenshiZ <angelant@ucm.es>
2024-10-24 13:19:26 +0200

    ApplicationController terminado

--- BGanosCOD/src/Presentacion/Controller/ApplicationController.java
+++ BGanosCOD/src/Presentacion/Controller/ApplicationController.java
@@ -1,47 +1,19 @@
-/**
- * 
- */
+
 package Presentacion.Controller;
 
 import Presentacion.Controller.Command.Context;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author airam
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
-public class ApplicationController {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+public abstract class ApplicationController {
+	
 	private static ApplicationController instance;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public static ApplicationController getInstance() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		if(instance == null){
+			instance = new ApplicationControllerImp();
+		}
+		return instance;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param context
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void manageRequest(Context context) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
+	public abstract void manageRequest(Context context);
+	
 }
