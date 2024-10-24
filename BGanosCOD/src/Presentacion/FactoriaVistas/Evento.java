a52eaecf41e8aa6a5cd955e7801f48e493c18f6f
TenshiZ <angelant@ucm.es>
2024-10-24 21:14:20 +0200

    Evento con KOs y Oks

--- BGanosCOD/src/Presentacion/FactoriaVistas/Evento.java
+++ BGanosCOD/src/Presentacion/FactoriaVistas/Evento.java
@@ -1,12 +1,10 @@
-
 package Presentacion.FactoriaVistas;
 
-
 public class Evento {
-    
+
     // Vistas Principales  								100 - 199
     public static final int VISTA_PRINCIPAL = 100;
-    
+
 	//	Entrada											200 - 299
 	    
 	//				Vistas Entrada						200 - 219
@@ -42,6 +40,9 @@ public class Evento {
 	public static final int LISTAR_ENTRADA_KO = 259;
 	
     
+	
+	
+	
     //				Fabricante							300 - 399
     
     //				Vistas Fabricante					300 - 319
@@ -56,7 +57,7 @@ public class Evento {
     public static final int LISTAR_FABRICANTES_EXTRANGEROS_VISTA = 308;
     public static final int LISTAR_INFORMACION_FABRICANTES_DE_SISTEMA_DE_RIEGO_DE_UN_INVERNADERO_VISTA = 309;
     
-    // 				Funciones Fabricante				320 - 399
+    // 				Funciones Fabricante				320 - 349
     public static final int ALTA_FABRICANTE = 320;
     public static final int BAJA_FABRICANTE = 321;
     public static final int MODIFICAR_FABRICANTE = 322;
@@ -66,6 +67,40 @@ public class Evento {
     public static final int LISTAR_FABRICANTES_LOCALES = 326;
     public static final int LISTAR_FABRICANTES_EXTRANGEROS = 327;
     public static final int LISTAR_INFORMACION_FABRICANTES_DE_SISTEMA_DE_RIEGO_DE_UN_INVERNADERO = 328;
+    
+    //				OKs y KOs Fabricante					350 - 399
+    //					Alta
+    public static final int ALTA_FABRICANTE_OK = 350;
+    public static final int ALTA_FABRICANTE_KO = 351;
+    //					Baja
+    public static final int BAJA_FABRICANTE_OK = 352;
+    public static final int BAJA_FABRICANTE_KO = 353;
+    //					Modificar
+    public static final int MODIFICAR_FABRICANTE_OK = 354;
+    public static final int MODIFICAR_FABRICANTE_KO = 355;
+    //					Listar
+    public static final int LISTAR_FABRICANTES_OK = 356;
+    public static final int LISTAR_FABRICANTES_KO = 357;
+    //					Mostrar
+    public static final int MOSTRAR_FABRICANTE_POR_ID_OK = 358;
+    public static final int MOSTRAR_FABRICANTE_POR_ID_KO = 359;
+    //					Listar por Nombre
+    public static final int LISTAR_FABRICANTES_POR_NOMBRE_OK = 360;
+    public static final int LISTAR_FABRICANTES_POR_NOMBRE_KO = 361;
+    //					Listar por Locales
+    public static final int LISTAR_FABRICANTES_LOCALES_OK = 362;
+    public static final int LISTAR_FABRICANTES_LOCALES_KO = 363;
+    //					Listar por Extrangeros
+    public static final int LISTAR_FABRICANTES_EXTRANGEROS_OK = 364;
+    public static final int LISTAR_FABRICANTES_EXTRANGEROS_KO = 365;
+    //					Listar por INFORMACION_FABRICANTES_DE_SISTEMA_DE_RIEGO_DE_UN_INVERNADERO
+    public static final int LISTAR_INFORMACION_FABRICANTES_DE_SISTEMA_DE_RIEGO_DE_UN_INVERNADERO_OK = 366;
+    public static final int LISTAR_INFORMACION_FABRICANTES_DE_SISTEMA_DE_RIEGO_DE_UN_INVERNADERO_KO = 367;
+    
+
+    
+    
+    
     
     
     //				Factura									400 - 499
@@ -79,17 +114,40 @@ public class Evento {
     public static final int LISTAR_FACTURAS_VISTA = 405;
     public static final int DEVOLVER_FACTURA_VISTA = 406;
     
-    //				Funciones Factura						420 - 499
+    //				Funciones Factura						420 - 449
     public static final int ABRIR_FACTURA = 420;
     public static final int CERRAR_FACTURA = 421;
     public static final int MODIFICAR_FACTURA = 422;
     public static final int MOSTRAR_FACTURA_POR_ID = 423;
     public static final int LISTAR_FACTURAS = 424;
     public static final int DEVOLVER_FACTURA = 425;
+    
+    //				OKs y KOs Factura					450 - 499
+    //					Abrir
+    public static final int ABRIR_FACTURA_OK = 450;
+    public static final int ABRIR_FACTURA_KO = 451;
+    //					Cerrar
+    public static final int CERRAR_FACTURA_OK = 452;
+    public static final int CERRAR_FACTURA_KO = 453;
+    //					Modificar
+    public static final int MODIFICAR_FACTURA_OK = 454;
+    public static final int MODIFICAR_FACTURA_KO = 455;
+    //					Mostrar
+    public static final int MOSTRAR_FACTURA_POR_ID_OK = 456;
+    public static final int MOSTRAR_FACTURA_POR_ID_KO = 457;
+    //					Listar
+    public static final int LISTAR_FACTURAS_OK = 458;
+    public static final int LISTAR_FACTURAS_KO = 459;
+    //					Devolver
+    public static final int DEVOLVER_FACTURA_OK = 460;
+    public static final int DEVOLVER_FACTURA_KO = 461;
 
 
     
-
+    
+    
+    
+    
     //				Invernadero									500 - 599
     
     //				Vistas Invernadero							500 - 519
@@ -104,7 +162,7 @@ public class Evento {
     public static final int DESVINCULAR_SISTEMA_RIEGO_INVERNADERO_VISTA = 508;
     public static final int CALCULAR_LAS_3_FECHAS_MAS_VENDIDAS_DE_UN_INVERNADERO_VISTA = 509;
     
-    //				Funciones Invernadero						520 - 599
+    //				Funciones Invernadero						520 - 549
     public static final int ALTA_INVERNADERO = 520;
     public static final int BAJA_INVERNADERO = 521;
     public static final int MODIFICAR_INVERNADERO = 522;
@@ -114,6 +172,42 @@ public class Evento {
     public static final int VINCULAR_SISTEMA_RIEGO_A_INVERNADERO = 526;
     public static final int DESVINCULAR_SISTEMA_RIEGO_DE_INVERNADERO = 527;
     public static final int CALCULAR_LAS_3_FECHAS_MAS_VENDIDAS_DE_UN_INVERNADERO = 528;
+    
+    //				OKs y KOs Invernadero					550 - 599
+    //					Alta
+    public static final int ALTA_INVERNADERO_OK = 550;
+    public static final int ALTA_INVERNADERO_KO = 551;
+    //					Baja
+    public static final int BAJA_INVERNADERO_OK = 552;
+    public static final int BAJA_INVERNADERO_KO = 553;
+    //					Modificar
+    public static final int MODIFICAR_INVERNADERO_OK = 554;
+    public static final int MODIFICAR_INVERNADERO_KO = 555;
+    //					Listar
+    public static final int LISTAR_INVERNADERO_OK = 556;
+    public static final int LISTAR_INVERNADERO_KO = 557;
+    //					Mostrar
+    public static final int MOSTRAR_INVERNADERO_POR_ID_OK = 558;
+    public static final int MOSTRAR_INVERNADERO_POR_ID_KO = 559;
+	//					Listar por sistema de riego
+	public static final int LISTAR_INVERNADEROS_POR_SISTEMA_RIEGO_OK = 560;
+	public static final int LISTAR_INVERNADEROS_POR_SISTEMA_RIEGO_KO = 561;
+	
+	//					Vincular sistema de riego a invernadero
+	public static final int VINCULAR_SISTEMA_RIEGO_A_INVERNADERO_OK = 562;
+	public static final int VINCULAR_SISTEMA_RIEGO_A_INVERNADERO_KO = 563;
+	
+	//					Desvincular sistema de riego de invernadero
+	public static final int DESVINCULAR_SISTEMA_RIEGO_DE_INVERNADERO_OK = 564;
+	public static final int DESVINCULAR_SISTEMA_RIEGO_DE_INVERNADERO_KO = 565;
+	
+	//					Calcular las 3 fechas más vendidas de un invernadero
+	public static final int CALCULAR_LAS_3_FECHAS_MAS_VENDIDAS_DE_UN_INVERNADERO_OK = 566;
+	public static final int CALCULAR_LAS_3_FECHAS_MAS_VENDIDAS_DE_UN_INVERNADERO_KO = 567;
+    
+      
+    
+    
     
     
     
@@ -129,7 +223,7 @@ public class Evento {
     public static final int LISTAR_PLANTAS_POR_TIPO_VISTA = 606;
     public static final int LISTAR_PLANTAS_INVERNADERO_VISTA = 607;
     
-    //				Funciones Planta							620 - 699
+    //				Funciones Planta							620 - 649
     public static final int ALTA_PLANTA = 620;
     public static final int BAJA_PLANTA = 621;
     public static final int MODIFICAR_PLANTA = 622;
@@ -137,6 +231,32 @@ public class Evento {
     public static final int MOSTRAR_PLANTA_POR_ID = 624;
     public static final int LISTAR_PLANTAS_POR_TIPO = 625;
     public static final int LISTAR_PLANTAS_DE_INVERNADERO = 626;
+    
+    //		OKs y KOs Planta					650 - 699
+    //					Alta
+    public static final int ALTA_PLANTA_OK = 650;
+    public static final int ALTA_PLANTA_KO = 651;
+    //					Baja
+    public static final int BAJA_PLANTA_OK = 652;
+    public static final int BAJA_PLANTA_KO = 653;
+    //					Modificar
+    public static final int MODIFICAR_PLANTA_OK = 654;
+    public static final int MODIFICAR_PLANTA_KO = 655;
+    //					Listar
+    public static final int LISTAR_PLANTAS_OK = 656;
+    public static final int LISTAR_PLANTAS_KO = 657;
+    //					Mostrar
+    public static final int MOSTRAR_PLANTA_POR_ID_OK = 658;
+    public static final int MOSTRAR_PLANTA_POR_ID_KO = 659;
+    //					Listar por Tipo
+    public static final int LISTAR_PLANTAS_POR_TIPO_OK = 660;
+    public static final int LISTAR_PLANTAS_POR_TIPO_KO = 661;
+    //					Listar de Invernadero
+    public static final int LISTAR_PLANTAS_DE_INVERNADERO_OK = 662;
+    public static final int LISTAR_PLANTAS_DE_INVERNADERO_KO = 663;
+    
+    
+    
     
     
 
@@ -182,17 +302,5 @@ public class Evento {
 	public static final int MOSTRAR_SISTEMA_DE_RIEGO_KO = 761;
 
 	
-
-	
-
-	
-
-	
-
-	
-
-	
-
-
 
 }
