f06a8b97080a8147dc11e7cb2893af0eb268a1b0
DiegoMontero747 <diemon01@ucm.es>
2024-10-26 16:18:05 +0200

    integracionFactura

--- BGanosCOD/src/Integracion/Factura/FacturaDAOImp.java
+++ BGanosCOD/src/Integracion/Factura/FacturaDAOImp.java
@@ -4,7 +4,18 @@
 package Integracion.Factura;
 
 import Negocio.Factura.TFactura;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.HashSet;
 import java.util.Set;
+
+import Integracion.Transaction.Transaccion;
+import Integracion.Transaction.TransaccionManager;
 
 /** 
 * <!-- begin-UML-doc -->
@@ -19,10 +30,24 @@ public class FacturaDAOImp implements FacturaDAO {
 	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
 	public Integer cerrarFactura(TFactura tfactura) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		TransaccionManager tManager = TransaccionManager.getInstance();
+		int exito = -1;
+		try {
+			Transaccion t = tManager.getTransaccion();
+			Connection c = (Connection) t.getResource();
+			PreparedStatement statement = c.prepareStatement(
+					"INSERT INTO factura(precio_total ,fecha_compra) VALUES (?,DATE_FORMAT(NOW(), '%Y-%m-%d'))", Statement.RETURN_GENERATED_KEYS);
+			statement.setDouble(1, tfactura.getPrecioTotal());
+			statement.executeUpdate();
+			ResultSet result = statement.getGeneratedKeys();
+			if (result.next())
+				exito = result.getInt(1);
+			statement.close();
+			result.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return exito;
 	}
 
 	/** 
@@ -31,10 +56,29 @@ public class FacturaDAOImp implements FacturaDAO {
 	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
 	public TFactura mostrarFactura(Integer id) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		TFactura factura = null;
+		try 
+		{
+			TransaccionManager tm = TransaccionManager.getInstance();
+			Transaccion t = tm.getTransaccion();
+			Connection c = (Connection) t.getResource();
+			PreparedStatement statement = c.prepareStatement("SELECT * FROM factura WHERE id = ? FOR UPDATE");
+			statement.setInt(1, id);
+			ResultSet result = statement.executeQuery();
+			if (result.next())
+			{
+				factura = new TFactura();
+				factura.setid(result.getInt(1));
+				factura.setPrecioTotal(result.getFloat(2));
+				factura.setFechaCompra(new Date(result.getDate(3).getTime()));
+				factura.setActivo(result.getBoolean(4));
+			}
+			statement.close();
+			result.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return factura;
 	}
 
 	/** 
@@ -43,10 +87,30 @@ public class FacturaDAOImp implements FacturaDAO {
 	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
 	public Set<TFactura> listarFactura() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		Set<TFactura> facturas = new HashSet<TFactura>();
+		try
+		{
+		Transaccion t = TransaccionManager.getInstance().getTransaccion();
+		Connection c = (Connection) t.getResource();
+		PreparedStatement statement = c.prepareStatement("SELECT * FROM factura FOR UPDATE");
+		ResultSet result = statement.executeQuery();
+		while (result.next())
+		{
+			TFactura factura = new TFactura();
+			factura.setid(result.getInt(1));
+			factura.setPrecioTotal(result.getFloat(2));
+			factura.setFechaCompra(new Date(result.getDate(3).getTime()));
+			factura.setActivo(result.getBoolean(4));
+			facturas.add(factura);
+		}
+	    statement.close();
+		result.close();	
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		return facturas;
 	}
 
 	/** 
@@ -55,10 +119,29 @@ public class FacturaDAOImp implements FacturaDAO {
 	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
 	public Integer modificarFactura(TFactura tfactura) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		int exito = -1;
+		try {
+			TransaccionManager tManager = TransaccionManager.getInstance();
+			Transaccion t = tManager.getTransaccion();
+			Connection c = (Connection) t.getResource();
+			PreparedStatement statement = c
+					.prepareStatement("UPDATE factura SET precio_total = ?, fecha_compra = ?, activo = ? WHERE id = ?");
+			
+			statement.setDouble(1, tfactura.getPrecioTotal());
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); // Formatear la fecha
+	        String formattedDate = dateFormat.format(tfactura.getFechaCompra()); // Obtener la fecha formateada como String
+	        statement.setString(2, formattedDate); // Establecer la fecha formateada en el PreparedStatement
+			statement.setBoolean(3, tfactura.getActivo());
+			statement.setInt(4, tfactura.getid());
+			exito = statement.executeUpdate();
+			statement.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if(exito != -1)
+        	return tfactura.getid();
+        else
+        	return exito;
 	}
 
 	/** 
@@ -67,9 +150,18 @@ public class FacturaDAOImp implements FacturaDAO {
 	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
 	public Integer devolverFactura(Integer id) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		int exito = -1;
+		try 
+		{
+			Transaccion t = TransaccionManager.getInstance().getTransaccion();
+			Connection c = (Connection) t.getResource();
+			Statement s = c.createStatement();
+			exito = s.executeUpdate("UPDATE factura SET precio_total = 0 WHERE id = " + id+";");
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		return exito;
 	}
 }
