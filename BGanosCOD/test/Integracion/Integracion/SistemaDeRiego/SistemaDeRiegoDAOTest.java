edfa796b28372c676842e85d877b9d21c5d580bb
TenshiZ <angelant@ucm.es>
2024-10-27 00:15:27 +0200

    Inicio SistemaDeRiegoDAOTest funciones aux

--- BGanosCOD/test/Integracion/Integracion/SistemaDeRiego/SistemaDeRiegoDAOTest.java
+++ BGanosCOD/test/Integracion/Integracion/SistemaDeRiego/SistemaDeRiegoDAOTest.java
@@ -2,13 +2,96 @@ package Integracion.SistemaDeRiego;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import Integracion.FactoriaIntegracion.FactoriaIntegracion;
+import Integracion.Transaction.Transaccion;
+import Integracion.Transaction.TransaccionManager;
+import Negocio.SistemaDeRiego.TSistemaDeRiego;
+
 public class SistemaDeRiegoDAOTest {
+	
+	private static SistemaDeRiegoDAO sistemaRiegoDAO;
+	
+	private boolean equals(TSistemaDeRiego s1, TSistemaDeRiego s2){
+		return true;
+	}
+	
+	private TSistemaDeRiego getTSistemaDeRiego() {
+
+		TSistemaDeRiego sistRiego = new TSistemaDeRiego();
+		
+
+		return sistRiego;
+	}
+	
+	private TSistemaDeRiego getTSistemaDeRiego(String nombreUnico) {
+
+		TSistemaDeRiego sistRiego = new TSistemaDeRiego();
+		
+
+		return sistRiego;
+	}
+	
+	private TSistemaDeRiego getTSistemaDeRiego(Integer idFabricante) {
+
+		TSistemaDeRiego sistRiego = new TSistemaDeRiego();
+		
+
+		return sistRiego;
+	}
+
+	private String getNameRandom() {
+		String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		StringBuilder nombreAleatorio = new StringBuilder();
+		Random random = new Random();
+		for (int i = 0; i < 10; i++) {
+			int index = random.nextInt(caracteres.length());
+			nombreAleatorio.append(caracteres.charAt(index));
+		}
+		return nombreAleatorio.toString();
+	}
+
+	private int getNumRandom(){
+		return ThreadLocalRandom.current().nextInt(0, 10000 + 1);
+	}
+
+	private Transaccion crearTransaccion() {
+		try {
+			TransaccionManager tManager = TransaccionManager.getInstance();
+			tManager.newTransaccion();
+			return tManager.getTransaccion();
+		} catch (Exception e) {
+			fail("Error transaccional");
+			return null;
+		}
+	}
+	
+	@BeforeClass
+	public static void beforeClass() {
+		sistemaRiegoDAO = FactoriaIntegracion.getInstance().getSistemaDeRiegoDAO();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAltaSistemaDeRiego() {
+		try {
+			Transaccion trans = crearTransaccion();
+			trans.start();
+			TSistemaDeRiego sistRiego = getTSistemaDeRiego();
+			Integer idSistemaDeRiego = sistemaRiegoDAO.altaSistemaDeRiego(sistRiego);
+			if (idSistemaDeRiego < 0) {
+				trans.rollback();
+				fail("Error: altaSistemaDeRiego() debería retornar ID > 0");
+			}
+			trans.commit();
+		} catch (Exception e) {
+			fail("Excepción");
+			e.printStackTrace();
+		}
 	}
 
 }
