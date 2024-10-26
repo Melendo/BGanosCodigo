d9ac81af1b50261413f6eb4266d2e93102b7f363
UCM-AliC <alichen@ucm.es>
2024-10-26 20:13:40 +0200

    cambios en los tplanta

--- BGanosCOD/src/Negocio/Planta/PlantaSAImp.java
+++ BGanosCOD/src/Negocio/Planta/PlantaSAImp.java
@@ -1,99 +1,40 @@
-/**
- * 
- */
 package Negocio.Planta;
 
-import java.util.Collection;
 import java.util.Set;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author airam
-* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-*/
 public class PlantaSAImp implements PlantaSA {
-	/** 
-	* (non-Javadoc)
-	* @see PlantaSA#altaPlanta(TPlanta planta)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	public Integer altaPlanta(TPlanta planta) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		if(planta.get_nombre().isEmpty() || planta.get_nombre_cientifico().isEmpty() || planta.get_id_invernadero().isEmpty()) {
+			return -3;
+		}else if(planta instanceof TPlantaFrutal) {
+			
+		}else if(planta instanceof TPlantaNoFrutal) {
+			
+		}
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see PlantaSA#bajaPlanta(Integer id)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Integer bajaPlanta(Integer id) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see PlantaSA#modificarPlanta(TPlanta planta)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Integer modificarPlanta(TPlanta planta) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see PlantaSA#listarPlanta()
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Set<TPlanta> listarPlanta() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see PlantaSA#mostrarPlantaPorId(Integer id)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public TPlanta mostrarPlantaPorId(Integer id) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see PlantaSA#listarPlantasPorTipo(String tipo)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Set<TPlanta> listarPlantasPorTipo(String tipo) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see PlantaSA#listarPlantasPorInvernadero(Integer id_invernadero)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Set<TPlanta> listarPlantasPorInvernadero(Integer id_invernadero) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+
 	}
 }
--- BGanosCOD/src/Negocio/Planta/TPlanta.java
+++ BGanosCOD/src/Negocio/Planta/TPlanta.java
@@ -3,38 +3,22 @@ package Negocio.Planta;
 public class TPlanta {
 
 	private String nombre_cientifico;
-
 	private String nombre;
-
 	private Integer id;
-
 	private Boolean activo;
-	
 	private Integer tipo;
-	
 	private Integer id_invernadero;
-
+	
 	public String get_nombre_cientifico() {
 		return nombre_cientifico;
 	}
+	
 	public String get_nombre() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return nombre;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Integer get_id() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return id;
 	}
 
 	public Integer get_tipo(){
@@ -44,37 +28,21 @@ public class TPlanta {
 	public Integer get_id_invernadero() {
 		return id_invernadero;
 	}
+	
+	public Boolean getActivo() {
+		return activo;
+	}
+	
 	public void set_nombre_cientifico(String nombreCientifico) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+		this.nombre_cientifico = nombreCientifico;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param nombre
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public void set_nombre(String nombre) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+		this.nombre = nombre;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param id
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public void set_id(Integer id) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+		this.id = id;
 	}
 
 	public void set_tipo(Integer tipo) {
@@ -85,37 +53,12 @@ public class TPlanta {
 		this.id_invernadero = id_invernadero;
 	}
 	
-	public String toString() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public Boolean getActivo() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param activo
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public void setActivo(Boolean activo) {
-		// begin-user-code
-		// TODO Auto-generated method stub
+		this.activo = activo;
+	}
+	
+	public String toString() {
 
-		// end-user-code
 	}
 	
 }
