90073892f13a0f7da7bf10541a98761df7e85667
IvanAlcCam <ivanalca@ucm.es>
2024-10-27 11:43:54 +0100

    Terminado Transfer TTiene

--- BGanosCOD/src/Negocio/Invernadero/TTiene.java
+++ BGanosCOD/src/Negocio/Invernadero/TTiene.java
@@ -3,49 +3,26 @@
  */
 package Negocio.Invernadero;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author airam
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public class TTiene {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private Integer id_Invernadero;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private Integer id_SistemasDeRiego;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public Integer getIdInvernadero() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+	public Integer getId_Invernadero() {
+		return id_Invernadero;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public Integer getIdSistemasDeRiego() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+	public void setId_Invernadero(Integer id_Invernadero) {
+		this.id_Invernadero = id_Invernadero;
 	}
+
+	public Integer getId_SistemasDeRiego() {
+		return id_SistemasDeRiego;
+	}
+
+	public void setId_SistemasDeRiego(Integer id_SistemasDeRiego) {
+		this.id_SistemasDeRiego = id_SistemasDeRiego;
+	}
+
 }
