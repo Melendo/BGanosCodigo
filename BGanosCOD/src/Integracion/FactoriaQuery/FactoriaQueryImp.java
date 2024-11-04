9143af18dcaac465999d0f7686f9cc7515e45397
IvanAlcCam <ivanalca@ucm.es>
2024-11-04 13:19:25 +0100

    Arreglado Factoria Query

--- BGanosCOD/src/Integracion/FactoriaQuery/FactoriaQueryImp.java
+++ BGanosCOD/src/Integracion/FactoriaQuery/FactoriaQueryImp.java
@@ -1,21 +1,18 @@
 package Integracion.FactoriaQuery;
 
-import Integracion.FactoriaQuery.calcularLasTresFechasMasVendidasDeUnInvernadero;
-import Integracion.FactoriaQuery.ListarInformacionFabricantePorSistemasDeRiegoDeUnInvernadero;
-
 public class FactoriaQueryImp extends FactoriaQuery {
-	
-	public Query getNewQuery(String nombre) {
-		
-		switch (nombre){
-		
-			case "calcularLasTresFechasMasVendidasDeUnInvernadero": 
-				return new calcularLasTresFechasMasVendidasDeUnInvernadero();
 
-			case "ListarInformacionFabricantePorSistemasDeRiegoDeUnInvernadero" : 
-				return new ListarInformacionFabricantePorSistemasDeRiegoDeUnInvernadero(); 
+	public Query getNewQuery(String nombre) {
+
+		switch (nombre) {
+
+		case "calcularLasTresFechasMasVendidasDeUnInvernadero":
+			return new calcularLasTresFechasMasVendidasDeUnInvernadero();
+
+		case "ListarInformacionFabricantePorSistemasDeRiegoDeUnInvernadero":
+			return new ListarInformacionFabricantePorSistemasDeRiegoDeUnInvernadero();
 		}
-		
+
 		return null;
 	}
 }
