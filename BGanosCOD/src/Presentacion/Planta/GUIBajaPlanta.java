6ebfc29993559dc85f0d33019e22813a3ce58bc5
comoquieras <jzhan02@ucm.es>
2024-11-06 16:17:24 +0100

    ventanas

--- BGanosCOD/src/Presentacion/Planta/GUIBajaPlanta.java
+++ BGanosCOD/src/Presentacion/Planta/GUIBajaPlanta.java
@@ -42,7 +42,7 @@ public class GUIBajaPlanta extends JFrame implements IGUI {
 		this.setBounds(x, y, ancho, alto);
 		this.setLayout(null);
 		this.setResizable(false);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		initGUI();
 	}
 	
