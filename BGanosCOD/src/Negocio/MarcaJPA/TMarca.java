87d144b715dd6905874cbc8ed62b349bdde16425
melanydece <melanydc@ucm.es>
2024-11-25 11:28:55 +0100

    alta marca presentación, TMarca

--- BGanosCOD/src/Negocio/MarcaJPA/TMarca.java
+++ BGanosCOD/src/Negocio/MarcaJPA/TMarca.java
@@ -1,137 +1,59 @@
-/**
- * 
- */
 package Negocio.MarcaJPA;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author airam
- * @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
- */
 public class TMarca {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+
 	private Integer id;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	private String PaisOrigen;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	private String Nombre;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	private Boolean Activo;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	private String paisOrigen;
+
+	private String nombre;
+
+	private Boolean activo;
+
+	public TMarca(Integer id, String nombre, String paisOrigen, Boolean activo) {
+		this.id = id;
+		this.nombre = nombre;
+		this.paisOrigen = paisOrigen;
+		this.activo = activo;
+	}
+
+	public TMarca(String nombre, String paisOrigen, Boolean activo) {
+		this.nombre = nombre;
+		this.paisOrigen = paisOrigen;
+		this.activo = activo;
+	}
+
 	public Integer getId() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return this.id;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public void setId() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	// TODO cambiar en todos los set el parámetro que se le pasa, pasarle el
+	// atributo a cambiar
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
 	public String getPais() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return this.paisOrigen;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public void setPais() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	public void setPais(String paisOrigen) {
+		this.paisOrigen = paisOrigen;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
 	public String getNombre() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return this.nombre;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public void setNombre() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
 	public Boolean getActivo() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return this.activo;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public void setActivo() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	public void setActivo(Boolean activo) {
+		this.activo = activo;
 	}
 }
--- BGanosCOD/src/Presentacion/MarcaJPA/GUIAltaMarca.java
+++ BGanosCOD/src/Presentacion/MarcaJPA/GUIAltaMarca.java
@@ -1,78 +1,151 @@
-/**
- * 
- */
 package Presentacion.MarcaJPA;
 
 import javax.swing.JFrame;
 import Presentacion.Controller.Command.Context;
+import Presentacion.FactoriaVistas.Evento;
+import Presentacion.ComponentsBuilder.ComponentsBuilder;
+import Presentacion.Controller.ApplicationController;
 import Presentacion.Controller.IGUI;
 import javax.swing.JTextField;
+
+import Negocio.MarcaJPA.TMarca;
+
 import javax.swing.JDialog;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author airam
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
 public class GUIAltaMarca extends JFrame implements IGUI, ActionListener {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+
+	private static final long serialVersionUID = 1L;
+
 	private JTextField jTextField;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+
 	private JDialog jDialog;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	private JButton jButton;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	private JPanel jPanel;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	private JLabel jLabel;
 
-	/** 
-	* (non-Javadoc)
-	* @see IGUI#actualizar(Context context)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public void actualizar(Context context) {
-		// begin-user-code
-		// TODO Auto-generated method stub
+	private JButton botonAceptar;
 
-		// end-user-code
+	private JButton botonCancelar;
+
+	private JPanel mainPanel;
+
+	// TODO nuevo, JLabel
+	private JLabel msgIntroIDCabecera;
+
+	public GUIAltaMarca() {
+		super("Alta marca");
+		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+		int ancho = 1000;
+		int alto = 525;
+		int x = (pantalla.width - ancho) / 2;
+		int y = (pantalla.height - alto) / 2;
+		this.setBounds(x, y, ancho, alto);
+		this.setLayout(null);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initGUI();
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see ActionListener#actionPerformed(ActionEvent e)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public void actionPerformed(ActionEvent e) {
-		// begin-user-code
-		// TODO Auto-generated method stub
+	public void initGUI() {
 
-		// end-user-code
+		// Panel principal
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		this.setContentPane(mainPanel);
+
+		mainPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+
+		msgIntroIDCabecera = ComponentsBuilder.createLabel("Introduzca los datos de la Marca que desea dar de alta", 1,
+				10, 80, 20, Color.BLACK);
+		msgIntroIDCabecera.setAlignmentX(CENTER_ALIGNMENT);
+		mainPanel.add(msgIntroIDCabecera);
+
+		mainPanel.add(Box.createRigidArea(new Dimension(0, 40)));
+
+		// Campo para introducir el nombre
+		JLabel labelNombre = new JLabel("Nombre de la marca: ");
+		labelNombre.setAlignmentX(CENTER_ALIGNMENT);
+		mainPanel.add(labelNombre);
+
+		JTextField textNombre = new JTextField(20);
+		textNombre.setMaximumSize(textNombre.getPreferredSize());
+		mainPanel.add(textNombre);
+
+		mainPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+
+		// Campo para introducir el país de origen
+		JLabel labelPaisOrigen = new JLabel("Pais de origen: ");
+		labelPaisOrigen.setAlignmentX(CENTER_ALIGNMENT);
+		mainPanel.add(labelPaisOrigen);
+
+		JTextField textPaisOrigen = new JTextField(20);
+		textPaisOrigen.setMaximumSize(textPaisOrigen.getPreferredSize());
+		mainPanel.add(textPaisOrigen);
+
+		mainPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+
+		// Panel para los botones
+		JPanel panelBotones = new JPanel();
+		mainPanel.add(panelBotones);
+
+		// BOTON ACEPTAR (GUARDAR LOS DATOS DEL ALTA)
+		botonAceptar = new JButton("Aceptar");
+		botonAceptar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				try {
+					// nombre y país de origen
+					String nombre = textNombre.getText();
+					String paisOrigen = textPaisOrigen.getText();
+
+					ApplicationController.getInstance().manageRequest(new Context(Evento.ALTA_MARCA,
+							new TMarca(nombre != null ? nombre : "", paisOrigen != null ? paisOrigen : "", true)));
+
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(GUIAltaMarca.this, "Error en el formato de los datos", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+
+			}
+
+		});
+
+		panelBotones.add(botonAceptar);
+
+		// BOTON CANCELAR
+		botonCancelar = new JButton("Cancelar");
+		botonCancelar.addActionListener(new ActionListener() {
+	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				GUIAltaMarca.this.setVisible(false);
+				ApplicationController.getInstance().manageRequest(new Context(Evento.MARCA_VISTA, null));
+			}
+		});
+		panelBotones.add(botonCancelar);
+	
+		this.setVisible(true);
+		this.setResizable(true);
+	
+	}
+
+	public void actualizar(Context context) {
+
+	}
+
+	// TODO no lo uso de momento
+	public void actionPerformed(ActionEvent e) {
+
 	}
 }
