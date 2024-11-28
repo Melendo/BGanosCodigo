00c217d3f1657abeb9922055ef509dfc6d51f87d
TenshiZ <angelant@ucm.es>
2024-11-28 02:04:23 +0100

    EmpleadoDeCajaSAImp limpio

--- BGanosCOD/src/Negocio/EmpleadoDeCajaJPA/EmpleadoDeCajaSAImp.java
+++ BGanosCOD/src/Negocio/EmpleadoDeCajaJPA/EmpleadoDeCajaSAImp.java
@@ -5,94 +5,45 @@ package Negocio.EmpleadoDeCajaJPA;
 
 import java.util.Set;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author airam
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
+
 public class EmpleadoDeCajaSAImp implements EmpleadoDeCajaSA {
-	/** 
-	* (non-Javadoc)
-	* @see EmpleadoDeCajaSA#altaEmpleadoDeCaja(TEmpleadoDeCaja empleado)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	
 	public Integer altaEmpleadoDeCaja(TEmpleadoDeCaja empleado) {
-		// begin-user-code
-		// TODO Auto-generated method stub
+		
 		return null;
-		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see EmpleadoDeCajaSA#bajaEmpleadoDeCaja()
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	
 	public Integer bajaEmpleadoDeCaja() {
-		// begin-user-code
-		// TODO Auto-generated method stub
+		
 		return null;
-		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see EmpleadoDeCajaSA#ModificarEmpleadoDeCaja()
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	
 	public Integer ModificarEmpleadoDeCaja() {
-		// begin-user-code
-		// TODO Auto-generated method stub
+
 		return null;
-		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see EmpleadoDeCajaSA#ListarEmpleadoDeCajaPorNombre(String nombre)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	
 	public Set<TEmpleadoDeCaja> ListarEmpleadoDeCajaPorNombre(String nombre) {
-		// begin-user-code
-		// TODO Auto-generated method stub
+		
 		return null;
-		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see EmpleadoDeCajaSA#MostrarEmpleadoDeCajaPorId(Integer id)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	
 	public Set<TEmpleadoDeCaja> MostrarEmpleadoDeCajaPorId(Integer id) {
-		// begin-user-code
-		// TODO Auto-generated method stub
+		
 		return null;
-		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see EmpleadoDeCajaSA#ListarEmpleadosPorTurno(Integer idTurno)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
 	public Set<TEmpleadoDeCaja> ListarEmpleadosPorTurno(Integer idTurno) {
-		// begin-user-code
-		// TODO Auto-generated method stub
+		
 		return null;
-		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see EmpleadoDeCajaSA#ListarEmpleadosDeCaja()
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
 	public Set<TEmpleadoDeCaja> ListarEmpleadosDeCaja() {
-		// begin-user-code
-		// TODO Auto-generated method stub
+		
 		return null;
-		// end-user-code
 	}
 }
