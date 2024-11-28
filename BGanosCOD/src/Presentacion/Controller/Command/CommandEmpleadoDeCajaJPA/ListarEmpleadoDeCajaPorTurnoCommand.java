d8875c55e0bbfa0202c4a77753985cfeedffc5e4
TenshiZ <angelant@ucm.es>
2024-11-28 17:18:09 +0100

    ListarEmpleadoDeCajaPorTurnoCommand terminado

--- BGanosCOD/src/Presentacion/Controller/Command/CommandEmpleadoDeCajaJPA/ListarEmpleadoDeCajaPorTurnoCommand.java
+++ BGanosCOD/src/Presentacion/Controller/Command/CommandEmpleadoDeCajaJPA/ListarEmpleadoDeCajaPorTurnoCommand.java
@@ -3,25 +3,31 @@
  */
 package Presentacion.Controller.Command.CommandEmpleadoDeCajaJPA;
 
+import java.util.Set;
+
+import Negocio.EmpleadoDeCajaJPA.TEmpleadoDeCaja;
+
+import Negocio.FactoriaNegocio.FactoriaSA;
+
 import Presentacion.Controller.Command.Command;
 import Presentacion.Controller.Command.Context;
+import Presentacion.FactoriaVistas.Evento;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author airam
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
 public class ListarEmpleadoDeCajaPorTurnoCommand implements Command {
-	/** 
-	* (non-Javadoc)
-	* @see Command#execute(Object datos)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+
+	
 	public Context execute(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		Set<TEmpleadoDeCaja> resultado = FactoriaSA.getInstance().getEmpleadoDeCajaJPA().ListarEmpleadosPorTurno((Integer) datos);
+		if(resultado.size() == 1){
+			TEmpleadoDeCaja empleado = resultado.iterator().next();
+			if (empleado.getID() <= 0)
+				return new Context(Evento.LISTAR_EMPLEADOS_DE_CAJA_POR_TURNO_KO,empleado);
+			else{
+				return new Context(Evento.LISTAR_EMPLEADOS_DE_CAJA_POR_TURNO_OK,resultado);
+			}
+		}else{
+			return new Context(Evento.LISTAR_EMPLEADOS_DE_CAJA_POR_TURNO_OK,resultado);
+		}
 	}
 }
+
