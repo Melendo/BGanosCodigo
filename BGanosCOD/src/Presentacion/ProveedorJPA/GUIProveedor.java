9d0cdf8b194d549a401016631159e9c17af71dae
IvanAlcCam <ivanalca@ucm.es>
2024-11-28 10:48:05 +0100

    Terminada GUI Proveedor

--- BGanosCOD/src/Presentacion/ProveedorJPA/GUIProveedor.java
+++ BGanosCOD/src/Presentacion/ProveedorJPA/GUIProveedor.java
@@ -3,107 +3,172 @@ package Presentacion.ProveedorJPA;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import Negocio.Invernadero.TInvernadero;
+import Negocio.ProveedorJPA.TProveedor;
 import Presentacion.ComponentsBuilder.ComponentsBuilder;
 import Presentacion.Controller.ApplicationController;
 import Presentacion.Controller.IGUI;
 import Presentacion.Controller.Command.Context;
 import Presentacion.FactoriaVistas.Evento;
+import Presentacion.Invernadero.GUIInvernadero;
 
 @SuppressWarnings("serial")
 public class GUIProveedor extends JFrame implements IGUI{
 
-	private JButton bAlta;
-	private JButton bBaja;
-	private JButton bModificar;
-	private JButton bMostrar;
-	private JButton bListar;
-	private JButton bAtras;
-	private JPanel mainPanel;
+	private TProveedor proveedor;
+
+	private JButton buttonAltaProveedor;
+    private JButton buttonBajaProveedor;
+    private JButton buttonModificarProveedor;
+    private JButton buttonMostrarProveedorPorID;
+    private JButton buttonListarProveedor;
+    private JButton buttonListarProveedorPorMarca;
+    private JButton buttonVincularProveedorMarca;
+    private JButton buttonDesvincularProveedorMarca;
+
+    private JButton backButton;
+    private JPanel panel;
 
 	public GUIProveedor() {
 		super("Proveedor");
-		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
-		int ancho = 800;
-		int alto = 500;
-		int x = (pantalla.width - ancho) / 2;
-		int y = (pantalla.height - alto) / 2;
-		this.setBounds(x, y, ancho, alto);
-		this.setLayout(null);
-		mainPanel = new JPanel();
-		this.setResizable(false);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		initGUI();
-		this.setVisible(true);
+		 Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+	        int ancho = 1000;
+	        int alto = 500;
+	        int x = (pantalla.width - ancho) / 2;
+	        int y = (pantalla.height - alto) / 2;
+	        this.setBounds(x, y, ancho, alto);
+	        this.setLayout(null);
+	        panel = new JPanel();
+	        this.setResizable(false);
+	        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        initGUI();
+	        this.setVisible(true);
 	}
 
 	private void initGUI() {
 
-		JLabel label = ComponentsBuilder.createLabel("Gestión de Turnos", 250, 30, 300, 50, Color.BLACK);
-		this.add(label);
+		JLabel label = ComponentsBuilder.createLabel("PROVEEDORES", 250, 30, 500, 50, Color.BLACK);
+        this.add(label);
 
-		// Alta Turno
-		bAlta = ComponentsBuilder.createButton("Alta Turno", 100, 100, 185, 100);
-		bAlta.setVisible(true);
-		this.add(bAlta);
-		bAlta.addActionListener(a -> {
-			this.setVisible(false);
-			ApplicationController.getInstance().manageRequest(new Context(Evento.ALTA_TURNO_VISTA, null));
-		});
+        // ALTA_INVERNADERO_VISTA
+        buttonAltaProveedor = ComponentsBuilder.createButton("Alta", 100, 100, 185, 100);
+        buttonAltaProveedor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GUIProveedor.this.setVisible(false);
+                ApplicationController.getInstance().manageRequest(new Context(Evento.ALTA_PROVEEDOR_VISTA, proveedor));
+            }
+        });
+        buttonAltaProveedor.setVisible(true);
+        this.add(buttonAltaProveedor);
 
-		// Baja Turno
-		bBaja = ComponentsBuilder.createButton("Baja Turno", 300, 100, 185, 100);
-		bBaja.setVisible(true);
-		this.add(bBaja);
-		bBaja.addActionListener(a -> {
-			this.setVisible(false);
-			ApplicationController.getInstance().manageRequest(new Context(Evento.BAJA_TURNO_VISTA, null));
-		});
+        // BAJA_INVERNADERO_VISTA
+        buttonBajaProveedor = ComponentsBuilder.createButton("Baja", 300, 100, 185, 100);
+        buttonBajaProveedor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	GUIProveedor.this.setVisible(false);
+                ApplicationController.getInstance().manageRequest(new Context(Evento.BAJA_PROVEEDOR_VISTA, proveedor));
+            }
+        });
+        buttonBajaProveedor.setVisible(true);
+        this.add(buttonBajaProveedor);
 
-		// Modificar Turno
-		bModificar = ComponentsBuilder.createButton("Modificar Turno", 500, 100, 185, 100);
-		bModificar.setVisible(true);
-		this.add(bModificar);
-		bModificar.addActionListener(a -> {
-			this.setVisible(false);
-			ApplicationController.getInstance().manageRequest(new Context(Evento.MODIFICAR_TURNO_VISTA, null));
-		});
+        // MODIFICAR_INVERNADERO_VISTA
+        buttonModificarProveedor = ComponentsBuilder.createButton("Modificar", 500, 100, 185, 100);
+        buttonModificarProveedor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	GUIProveedor.this.setVisible(false);
+                ApplicationController.getInstance().manageRequest(new Context(Evento.MODIFICAR_PROVEEDOR_VISTA, proveedor));
+            }
+        });
+        buttonModificarProveedor.setVisible(true);
+        this.add(buttonModificarProveedor);
 
-		// Mostrar Turno
-		bMostrar = ComponentsBuilder.createButton("Mostrar Turno", 100, 250, 185, 100);
-		bMostrar.setVisible(true);
-		this.add(bMostrar);
-		bMostrar.addActionListener(a -> {
-			this.setVisible(false);
-			ApplicationController.getInstance().manageRequest(new Context(Evento.MOSTRAR_TURNO_VISTA, null));
-		});
+        // MOSTRAR_INVERNADERO_POR_ID_VISTA
+        buttonMostrarProveedorPorID = ComponentsBuilder.createButton("Mostrar por ID", 700, 100, 185, 100);
+        buttonMostrarProveedorPorID.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	GUIProveedor.this.setVisible(false);
+                ApplicationController.getInstance().manageRequest(new Context(Evento.MOSTRAR_PROVEEDORES_POR_ID_VISTA, proveedor));
+            }
+        });
+        buttonMostrarProveedorPorID.setVisible(true);
+        this.add(buttonMostrarProveedorPorID);
 
-		// Listar Turnos
-		bListar = ComponentsBuilder.createButton("Listar Turnos", 300, 250, 185, 100);
-		bListar.setVisible(true);
-		this.add(bListar);
-		bListar.addActionListener(a -> {
-			this.setVisible(false);
-			ApplicationController.getInstance().manageRequest(new Context(Evento.LISTAR_TURNO_VISTA));
-		});
+        // LISTAR_INVERNADEROS
+        buttonListarProveedor = ComponentsBuilder.createButton("Listar", 100, 250, 185, 100);
+        buttonListarProveedor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	GUIProveedor.this.setVisible(false);
+                ApplicationController.getInstance().manageRequest(new Context(Evento.LISTAR_PROVEEDORES_VISTA, proveedor));
+            }
+        });
+        buttonListarProveedor.setVisible(true);
+        this.add(buttonListarProveedor);
 
+        // LISTAR_INVERNADEROS_POR_SISTEMA_RIEGO_VISTA
+        buttonListarProveedorPorMarca = ComponentsBuilder.createButton("Listar por Marca", 300, 250, 185, 100);
+        buttonListarProveedorPorMarca.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	GUIProveedor.this.setVisible(false);
+                ApplicationController.getInstance().manageRequest(new Context(Evento.LISTAR_PROVEEDORES_DE_MARCA, proveedor));
+            }
+        });
+        buttonListarProveedorPorMarca.setVisible(true);
+        this.add(buttonListarProveedorPorMarca);
+        
+        // VINCULAR_SISTEMA_RIEGO_INVERNADERO_VISTA
+        buttonVincularProveedorMarca = ComponentsBuilder.createButton("Vincular Marca", 500, 250, 185, 100);
+        buttonVincularProveedorMarca.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	GUIProveedor.this.setVisible(false);
+                ApplicationController.getInstance().manageRequest(new Context(Evento.VINCULAR_MARCA_PROVEEDOR_VISTA, proveedor));
+            }
+        });
+        buttonVincularProveedorMarca.setVisible(true);
+        this.add(buttonVincularProveedorMarca);
+        
+     // DESVINCULAR_SISTEMA_RIEGO_INVERNADERO_VISTA
+        buttonDesvincularProveedorMarca = ComponentsBuilder.createButton("Desvincular Marca", 700, 250, 185, 100);
+        buttonDesvincularProveedorMarca.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	GUIProveedor.this.setVisible(false);
+                ApplicationController.getInstance().manageRequest(new Context(Evento.DESVINCULAR_MARCA_PROVEEDOR_VISTA, proveedor));
+            }
+        });
+        buttonDesvincularProveedorMarca.setVisible(true);
+        this.add(buttonDesvincularProveedorMarca);
+        
 
-		// Botón Atrás
-		bAtras = ComponentsBuilder.createBackButton();
-		bAtras.setVisible(true);
-		this.add(bAtras);
-		bAtras.addActionListener(a -> {
-			GUIProveedor.this.setVisible(false);
-			ApplicationController.getInstance().manageRequest(new Context(Evento.VISTA_PRINCIPAL, null));
-			dispose();
-		});
+        // BOT�N DE VOLVER
+        backButton = ComponentsBuilder.createBackButton();
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	GUIProveedor.this.setVisible(false);
+                ApplicationController.getInstance().manageRequest(new Context(Evento.VISTA_PRINCIPAL, null));
+                dispose();
+            }
+        });
+        backButton.setVisible(true);
+        this.add(backButton);
 
-		getContentPane().add(mainPanel);
+        getContentPane().add(panel);
 	}
 
 	@Override
