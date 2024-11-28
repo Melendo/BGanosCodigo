1dff862a94762b47fa9b09611d438524d2908d31
TenshiZ <angelant@ucm.es>
2024-11-28 11:11:15 +0100

    TEmpleadoParcial terminado

--- BGanosCOD/src/Negocio/EmpleadoDeCajaJPA/TEmpleadoParcial.java
+++ BGanosCOD/src/Negocio/EmpleadoDeCajaJPA/TEmpleadoParcial.java
@@ -1,77 +1,32 @@
-/**
- * 
- */
+
 package Negocio.EmpleadoDeCajaJPA;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author airam
- * @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
- */
 public class TEmpleadoParcial extends TEmpleadoDeCaja {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	
 	private Double Precio_h;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
 	private Double Horas;
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public Double getHoras() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+	
+	public TEmpleadoParcial(Integer id, String dni, String nombre, String apellido, Double sueldo, Integer tlf,
+			Integer id_turno, Boolean activo, Double precio_h, Double horas) {
+		super(id, dni, nombre, apellido, sueldo, tlf, id_turno, activo, 1); // 1 sera empleado Parcial
+		this.setPrecio_h(precio_h);
+		this.setHoras(horas);
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param horas
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	public void setHoras(Double horas) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
 	public Double getPrecio_h() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return Precio_h;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param precio_h
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
 	public void setPrecio_h(Double precio_h) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+		Precio_h = precio_h;
 	}
+
+	public Double getHoras() {
+		return Horas;
+	}
+
+	public void setHoras(Double horas) {
+		Horas = horas;
+	}
+	
 }
