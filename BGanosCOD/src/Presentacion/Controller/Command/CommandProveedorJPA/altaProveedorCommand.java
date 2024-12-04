4c9e04eb91e65b256e4abb305a4c9fe9b7efb852
IvanAlcCam <ivanalca@ucm.es>
2024-12-04 18:46:59 +0100

    Eliminado import y formateo de codigo

--- BGanosCOD/src/Presentacion/Controller/Command/CommandProveedorJPA/altaProveedorCommand.java
+++ BGanosCOD/src/Presentacion/Controller/Command/CommandProveedorJPA/altaProveedorCommand.java
@@ -4,18 +4,16 @@
 package Presentacion.Controller.Command.CommandProveedorJPA;
 
 import Negocio.FactoriaNegocio.FactoriaNegocio;
-import Negocio.Invernadero.TInvernadero;
 import Negocio.ProveedorJPA.TProveedor;
 import Presentacion.Controller.Command.Command;
 import Presentacion.Controller.Command.Context;
 import Presentacion.FactoriaVistas.Evento;
 
-
 public class altaProveedorCommand implements Command {
-	
+
 	public Context execute(Object datos) {
-int resultado = FactoriaNegocio.getInstance().getProveedorJPA().altaProveedor((TProveedor) datos);
-		
+		int resultado = FactoriaNegocio.getInstance().getProveedorJPA().altaProveedor((TProveedor) datos);
+
 		if (resultado > -1) {
 			return new Context(Evento.ALTA_PROVEEDOR_OK, resultado);
 		} else {
