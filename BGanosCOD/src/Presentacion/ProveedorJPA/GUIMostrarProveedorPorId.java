3f077cf3fda902a7c1928bd603f5c2c7dbbfaa42
IvanAlcCam <ivanalca@ucm.es>
2024-12-04 16:19:50 +0100

    Terminada Vista Mostrar Proveedor por ID (falta logica)

--- BGanosCOD/src/Presentacion/ProveedorJPA/GUIMostrarProveedorPorId.java
+++ BGanosCOD/src/Presentacion/ProveedorJPA/GUIMostrarProveedorPorId.java
@@ -4,50 +4,111 @@
 package Presentacion.ProveedorJPA;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import Presentacion.Controller.Command.Context;
+import Presentacion.FactoriaVistas.Evento;
+import Presentacion.Invernadero.GUIMostrarInvernaderoPorID;
+import Presentacion.Controller.ApplicationController;
 import Presentacion.Controller.IGUI;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JDialog;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author airam
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
-public class GUIMostrarProveedorPorId extends JFrame implements IGUI {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	private JButton jButton;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	private JPanel jPanel;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	private JTextField jTextField;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
-	private JDialog jDialog;
 
-	/** 
-	* (non-Javadoc)
-	* @see IGUI#actualizar(Context context)
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+public class GUIMostrarProveedorPorId extends JFrame implements IGUI {
+	private JTextField textId; // Campo para introducir el ID del sistema de riego
+
+	public GUIMostrarProveedorPorId() {
+		super("Mostrar Proveedore por ID");
+		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+		int ancho = 600;
+		int alto = 400;
+		int x = (pantalla.width - ancho) / 2;
+		int y = (pantalla.height - alto) / 2;
+		this.setBounds(x, y, ancho, alto);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initGUI();
+	}
+
+	public void initGUI() {
+		// Panel principal con GridBagLayout para mayor control sobre la alineacion y el
+		// centrado
+		JPanel mainPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(10, 10, 10, 10); // Margenes entre los componentes
+		this.setContentPane(mainPanel);
+
+		// Titulo
+		gbc.gridwidth = 2; // Toma dos columnas para el titulo
+		JLabel msgIntro = new JLabel("Introduzca el ID del Proveedor a mostrar", JLabel.CENTER);
+		mainPanel.add(msgIntro, gbc);
+
+		// Resetear para los campos
+		gbc.gridwidth = 1;
+		gbc.gridy = 1;
+
+		// Campo para el ID del Proveedor
+		JLabel labelId = new JLabel("ID:");
+		gbc.gridx = 0; // Columna 0
+		mainPanel.add(labelId, gbc);
+		textId = new JTextField(20);
+		gbc.gridx = 1; // Columna 1
+		mainPanel.add(textId, gbc);
+
+		// Panel de botones
+		JPanel panelBotones = new JPanel();
+		gbc.gridx = 0;
+		gbc.gridy = 6;
+		gbc.gridwidth = 2; // Los botones ocupar�n dos columnas
+		gbc.anchor = GridBagConstraints.CENTER; // Centrar los botones
+		mainPanel.add(panelBotones, gbc);
+
+		// Bot�n de aceptar
+		JButton botonAceptar = new JButton("Aceptar");
+		botonAceptar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Integer idTexto = Integer.parseInt(textId.getText());
+
+					// Crear un contexto con el evento de mostrar y el ID del sistema
+					ApplicationController.getInstance()
+							.manageRequest(new Context(Evento.MOSTRAR_PROVEEDORES_POR_ID, idTexto));
+				} catch (NumberFormatException ex) {
+					JOptionPane.showMessageDialog(GUIMostrarProveedorPorId.this, "Error en el formato del ID",
+							"Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		panelBotones.add(botonAceptar);
+
+		// Boton de cancelar
+		JButton botonCancelar = new JButton("Cancelar");
+		botonCancelar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				GUIMostrarProveedorPorId.this.setVisible(false);
+				ApplicationController.getInstance().manageRequest(new Context(Evento.PROVEEDOR_VISTA, null));
+			}
+		});
+		panelBotones.add(botonCancelar);
+
+		this.setVisible(true);
+	}
 	public void actualizar(Context context) {
 		// begin-user-code
 		// TODO Auto-generated method stub
