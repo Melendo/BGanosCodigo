18b6e14b389412f2b9a11e47fe9a02e2b4cd1d07
Luis <Luisre01@ucm.es>
2024-12-06 18:56:44 +0100

    guiempleadosporid
    commit conflictos

--- BGanosCOD/src/Presentacion/EmpleadoDeCajaJPA/GUIMostrarEmpleadoDeCajaPorId.java
+++ BGanosCOD/src/Presentacion/EmpleadoDeCajaJPA/GUIMostrarEmpleadoDeCajaPorId.java
@@ -1,33 +1,5 @@
-/**
- * 
- */
 package Presentacion.EmpleadoDeCajaJPA;
 
-import javax.swing.JFrame;
-import Presentacion.Controller.Command.Context;
-import Presentacion.Controller.IGUI;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JTextField;
+public class GUIMostrarEmpleadoDeCajaPorId {
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author airam
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
-public class GUIMostrarEmpleadoDeCajaPorId extends JFrame implements IGUI {
-	private JDialog jDialog;
-	private JLabel jLabel;
-	private JPanel jPanel;
-	private JButton jButton;
-	private JTextField jTextField;
-	public void actualizar(Context context) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
 }
