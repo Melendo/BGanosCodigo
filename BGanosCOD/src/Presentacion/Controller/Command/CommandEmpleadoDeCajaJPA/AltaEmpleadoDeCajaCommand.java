deb8d6936753a3b979a7e5a8512f1c4ab2037653
TenshiZ <angelant@ucm.es>
2024-12-08 14:57:27 +0100

    CambioNombres Factorias EmpleadoDeCaja y arreglos

--- BGanosCOD/src/Presentacion/Controller/Command/CommandEmpleadoDeCajaJPA/AltaEmpleadoDeCajaCommand.java
+++ BGanosCOD/src/Presentacion/Controller/Command/CommandEmpleadoDeCajaJPA/AltaEmpleadoDeCajaCommand.java
@@ -2,6 +2,7 @@
 package Presentacion.Controller.Command.CommandEmpleadoDeCajaJPA;
 
 import Negocio.EmpleadoDeCajaJPA.TEmpleadoDeCaja;
+import Negocio.FactoriaNegocio.FactoriaNegocio;
 import Negocio.FactoriaNegocio.FactoriaSA;
 import Presentacion.Controller.Command.Command;
 import Presentacion.Controller.Command.Context;
@@ -11,7 +12,10 @@ import Presentacion.FactoriaVistas.Evento;
 public class AltaEmpleadoDeCajaCommand implements Command {
 	
 	public Context execute(Object datos) {
-		int res = FactoriaSA.getInstance().getEmpleadoDeCajaJPA().altaEmpleadoDeCaja((TEmpleadoDeCaja) datos);
+		
+
+		int res = FactoriaNegocio.getInstance().getEmpleadoDeCajaJPA().altaEmpleadoDeCaja((TEmpleadoDeCaja) datos);
+		System.out.println("HECHOOO");
 		if (res > -1) {
 			return new Context(Evento.ALTA_EMPLEADO_DE_CAJA_OK, res);
 		} else {
--- BGanosCOD/src/Presentacion/Controller/Command/CommandEmpleadoDeCajaJPA/MostrarEmpleadoDeCajaPorIdCommand.java
+++ BGanosCOD/src/Presentacion/Controller/Command/CommandEmpleadoDeCajaJPA/MostrarEmpleadoDeCajaPorIdCommand.java
@@ -4,6 +4,7 @@
 package Presentacion.Controller.Command.CommandEmpleadoDeCajaJPA;
 
 import Negocio.EmpleadoDeCajaJPA.TEmpleadoDeCaja;
+import Negocio.FactoriaNegocio.FactoriaNegocio;
 import Negocio.FactoriaNegocio.FactoriaSA;
 import Presentacion.Controller.Command.Command;
 import Presentacion.Controller.Command.Context;
@@ -13,7 +14,7 @@ import Presentacion.FactoriaVistas.Evento;
 public class MostrarEmpleadoDeCajaPorIdCommand implements Command {
 	
 	public Context execute(Object datos) {
-		TEmpleadoDeCaja res = FactoriaSA.getInstance().getEmpleadoDeCajaJPA().MostrarEmpleadoDeCajaPorId((int) datos);
+		TEmpleadoDeCaja res = FactoriaNegocio.getInstance().getEmpleadoDeCajaJPA().MostrarEmpleadoDeCajaPorId((int) datos);
 		if (res.getID() > -1) {
 			return new Context(Evento.MOSTAR_EMPLEADO_DE_CAJA_POR_ID_OK, res);
 		} else {
