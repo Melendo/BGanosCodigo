939f26666fbe98f7c5ac19f68c2bfb9a290194f2
IvanAlcCam <ivanalca@ucm.es>
2024-12-08 13:52:35 +0100

    Arreglado Command listar Proveedores de marca

--- BGanosCOD/src/Presentacion/Controller/Command/CommandProveedorJPA/listarProveedoresDeMarcaCommand.java
+++ BGanosCOD/src/Presentacion/Controller/Command/CommandProveedorJPA/listarProveedoresDeMarcaCommand.java
@@ -13,8 +13,8 @@ public class listarProveedoresDeMarcaCommand implements Command {
 	public Context execute(Object datos) {
 		Set<TProveedor> resultado = (Set<TProveedor>) FactoriaNegocio.getInstance().getProveedorJPA()
 				.listarProveedoresDeMarca((Integer) datos);
-		if (resultado.size() == 1) {
-			if (resultado.iterator().next().getId() == -1) {
+		if (resultado.size() >= 1) {
+			if (resultado.iterator().next().getId() > 0) {
 				return new Context(Evento.LISTAR_PROVEEDORES_DE_MARCA_OK, resultado);
 			}
 		}
